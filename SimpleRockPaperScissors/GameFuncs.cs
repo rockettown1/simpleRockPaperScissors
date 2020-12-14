688128fee96611de31e1bb29dfbb25ef8cd4bb9a
Dan Krishnan <dan@wearecodenation.com>
2020-12-14 18:43:45 +0000

    Testing VS for Mac

---
+++ SimpleRockPaperScissors/GameFuncs.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace SimpleRockPaperScissors
+{
+    public class GameFuncs
+    {
+
+        public int wins { get; set; }
+
+        public GameFuncs()
+        {
+            this.wins = 0;
+        }
+
+
+        public void CalcWin(int ranNum, string[] options, string playerChoice, int plays)
+            {
+            if (playerChoice == options[ranNum])
+            {
+                Console.WriteLine("Hmm a draw");
+            }
+            else if (playerChoice == "rock")
+            {
+                if (ranNum == 2)
+                {
+                    Console.WriteLine("Ahh well played");
+                    wins += 1;
+                    Console.WriteLine($"You've won {wins}/{plays}");
+                }
+                else
+                {
+                    Console.WriteLine("ah unlucky this time!");
+                }
+            }
+            else if (playerChoice == "paper")
+            {
+                if (ranNum == 0)
+                {
+                    Console.WriteLine("Ahh well played");
+                    wins += 1;
+                    Console.WriteLine($"You've won {wins}/{plays}");
+                }
+                else
+                {
+                    Console.WriteLine("ah unlucky this time!");
+                }
+            }
+            else if (playerChoice == "scissors")
+            {
+                if (ranNum == 1)
+                {
+                    Console.WriteLine("Ahh well played");
+                    wins += 1;
+                    Console.WriteLine($"You've won {wins}/{plays}");
+                }
+                else
+                {
+                    Console.WriteLine("ah unlucky this time!");
+                }
+            }
+        }
+    }
+}
---
+++ SimpleRockPaperScissors/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SimpleRockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] options = { "rock", "paper", "scissors" };
+            GameFuncs Logic = new GameFuncs();
+
+
+            //Ask how many goes users wants
+            Console.WriteLine("How many times do you want to play?");
+            if (int.TryParse(Console.ReadLine(), out int plays))
+            {
+                Console.WriteLine($"Ok, let's play {plays} times");
+            }
+            else
+            {
+                Console.WriteLine("Please type a number!");
+            }
+
+
+            //Run the game loop for chosen number of times
+            for (int i = 0; i < plays; i++)
+            {
+
+                //Ensure player chooses one of the options
+                string playerChoice = "";
+                bool chosen = false;
+                while (!chosen)
+                {
+                    Console.WriteLine("Rock, Paper, or Scissors? Choose your weapon!");
+                    playerChoice = Console.ReadLine().ToLower();
+                    int choice = Array.IndexOf(options, playerChoice);
+                    if (choice >= 0)
+                    {
+                        chosen = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("That's not one of the options!");
+                    }
+
+                }
+
+
+                int ranNum = new Random().Next(0, 3);
+                Console.WriteLine($"{playerChoice} VS {options[ranNum]}");
+
+                
+                Logic.CalcWin(ranNum, options, playerChoice, plays);
+                
+            }
+
+            Console.WriteLine($"*** You won {Logic.wins} out of {plays} ***");
+
+            if (Logic.wins > ((plays / 2) + 1))
+            {
+                Console.WriteLine("You're the winner!");
+            } else
+            {
+                Console.WriteLine("Ouch, you're not very good at this");
+            }
+            
+        }
+    }
+}
